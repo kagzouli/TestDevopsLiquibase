09f0394fd7b76ebed9af9e897ae33281e16c7a6b
kagzouli <karim@exakaconsulting.com>
2026-01-11 17:25:06 +0100

    Init

---
+++ createdabase.sql
@@ -0,0 +1,11 @@
+-- CREATE DATABASE
+CREATE DATABASE testliquibase;
+
+-- Grant all privileges on testliquibase database
+GRANT ALL PRIVILEGES ON testliquibase.* TO 'liquibase'@'localhost';
+
+-- Apply changes
+FLUSH PRIVILEGES;
+
+-- Verify the grant
+SHOW GRANTS FOR 'liquibase'@'localhost';
